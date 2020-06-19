b6ead4fe415ad41924de2ff5f93b4ae95227f8f9
andelkov <64714084+andelkov@users.noreply.github.com>
2020-06-19 22:14:07 +0000

    Add files via upload

---
+++ QuizApp/AppDelegate.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = AuthorizationUtils.isUserLoggedIn() ? TabBarViewController() : LoginViewController()
+        
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+    
+    func switchToLoginController() {
+        self.window?.rootViewController = LoginViewController()
+    }
+    
+    func switchToTabBarController() {
+        self.window?.rootViewController = TabBarViewController()
+    }
+
+}
+
